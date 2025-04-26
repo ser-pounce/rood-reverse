63218bd692adaefa94f866fb5cdad97f0030d9a7
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-04-26 14:02:17 +0200

    Move file split

--- src/SLUS_010.40/31724.c
+++ src/SLUS_010.40/31724.c
@@ -1,5 +1,4 @@
 #include "common.h"
-#include "31724.h"
 
 INCLUDE_ASM("build/src/SLUS_010.40/nonmatchings/31724", func_80040F24);
 
@@ -10,3 +9,7 @@ INCLUDE_ASM("build/src/SLUS_010.40/nonmatchings/31724", func_80041104);
 INCLUDE_ASM("build/src/SLUS_010.40/nonmatchings/31724", func_8004137C);
 
 INCLUDE_ASM("build/src/SLUS_010.40/nonmatchings/31724", func_8004140C);
+
+INCLUDE_RODATA("build/src/SLUS_010.40/nonmatchings/31724", D_80040210);
+
+INCLUDE_RODATA("build/src/SLUS_010.40/nonmatchings/31724", D_80040A14);

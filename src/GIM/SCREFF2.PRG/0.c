c1873cbe121a717bec741099c501af9d91863c80
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-05-01 15:46:57 +0200

    Split SCREFF2.PRG

---
+++ src/GIM/SCREFF2.PRG/0.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+INCLUDE_ASM("build/src/GIM/SCREFF2.PRG/nonmatchings/0", func_800F9800);
+
+INCLUDE_ASM("build/src/GIM/SCREFF2.PRG/nonmatchings/0", func_800F986C);
+
+INCLUDE_ASM("build/src/GIM/SCREFF2.PRG/nonmatchings/0", func_800F9BC0);
+
+INCLUDE_ASM("build/src/GIM/SCREFF2.PRG/nonmatchings/0", func_800F9BD8);
+
+INCLUDE_ASM("build/src/GIM/SCREFF2.PRG/nonmatchings/0", func_800F9DE8);
+
+INCLUDE_ASM("build/src/GIM/SCREFF2.PRG/nonmatchings/0", func_800F9DF8);
+
+INCLUDE_ASM("build/src/GIM/SCREFF2.PRG/nonmatchings/0", func_800F9EBC);
+
+INCLUDE_ASM("build/src/GIM/SCREFF2.PRG/nonmatchings/0", func_800F9FB8);

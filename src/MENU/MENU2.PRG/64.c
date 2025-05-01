51c423dbc67437a91905a04aa0b1187b31f9e588
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-05-01 22:17:36 +0200

    Split MENU2.PRG

---
+++ src/MENU/MENU2.PRG/64.c
@@ -0,0 +1,25 @@
+#include "common.h"
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80102864);
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80102928);
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80102B5C);
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80102CAC);
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_801034FC);
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80103670);
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_801037E4);
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_801038D4);
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80103C3C);
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80103DD8);
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80103E20);
+
+INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80104578);

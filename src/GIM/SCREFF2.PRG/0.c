2f26a1a35a8d5dc80fe41446ffa2f41004e00f07
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-05-01 18:23:28 +0200

    func_800F9FB8

--- src/GIM/SCREFF2.PRG/0.c
+++ src/GIM/SCREFF2.PRG/0.c
@@ -24,6 +24,7 @@ void func_8007DFF0(int, int, int);
 void func_8007E0A8(int, int, int);
 
 extern D_800EB9B4_t* D_800EB9B4;
+extern int* D_1F800000[];
 
 void func_800F9800(int arg0)
 {
@@ -65,22 +66,49 @@ int func_800F9DF8(int arg0, int arg1)
         | ((((((vs_math_sine(arg0 << 6) * arg1) >> 0xC) * var_s1) >> 6) + 0x70) << 0x10);
 }
 
-func_800F9EBC_t* func_800F9EBC(int arg0, int arg1, func_800F9EBC_t* arg2, u_int* arg3)
+int* func_800F9EBC(int arg0, int arg1, int* arg2, int* arg3)
 {
     int var_s2;
 
     for (var_s2 = arg1; var_s2 < arg1 + 4; ++var_s2) {
-        arg2->unk0 = (*arg3 & 0xFFFFFF) | 0x06000000;
-        arg2->unk4 = 0x32FFFFFF;
-        arg2->unk8 = arg0;
-        arg2->unkC = 0xA0A0A0;
-        arg2->unk10 = func_800F9DF8(var_s2 + 1, 0x80);
-        arg2->unk14 = 0xA0A0A0;
-        arg2->unk18 = func_800F9DF8(var_s2, 0x80);
+        arg2[0] = (*arg3 & 0xFFFFFF) | 0x06000000;
+        arg2[1] = 0x32FFFFFF;
+        arg2[2] = arg0;
+        arg2[3] = 0xA0A0A0;
+        arg2[4] = func_800F9DF8(var_s2 + 1, 0x80);
+        arg2[5] = 0xA0A0A0;
+        arg2[6] = func_800F9DF8(var_s2, 0x80);
         *arg3 = (u_int)((long)arg2 << 8) >> 8;
-        arg2 += 1;
+        arg2 += 7;
     }
     return arg2;
 }
 
-INCLUDE_ASM("build/src/GIM/SCREFF2.PRG/nonmatchings/0", func_800F9FB8);
+void func_800F9FB8()
+{
+    int* var_s2;
+    int i;
+    int* temp_s4;
+
+    temp_s4 = D_1F800000[2];
+    var_s2 = func_800F9EBC(0xE00140, 6, D_1F800000[0], temp_s4);
+    var_s2 = func_800F9EBC(0xE00000, 0x16, var_s2, temp_s4);
+    var_s2 = func_800F9EBC(0, 0x26, var_s2, temp_s4);
+    var_s2 = func_800F9EBC(0x140, 0x36, var_s2, temp_s4);
+
+    for (i = 0; i < 0x40; ++i) {
+        var_s2[0] = (*temp_s4 & 0xFFFFFF) | 0x09000000;
+        var_s2[1] = 0xE1000220;
+        var_s2[2] = 0x3AA0A0A0;
+        var_s2[3] = func_800F9DF8(i + 1, 0x80);
+        var_s2[4] = 0xA0A0A0;
+        var_s2[5] = func_800F9DF8(i, 0x80);
+        var_s2[6] = 0;
+        var_s2[7] = func_800F9DF8(i + 1, 0x60);
+        var_s2[8] = 0;
+        var_s2[9] = func_800F9DF8(i, 0x60);
+        *temp_s4 = (u_int)((long)var_s2 << 8) >> 8;
+        var_s2 += 10;
+    }
+    D_1F800000[0] = var_s2;
+}

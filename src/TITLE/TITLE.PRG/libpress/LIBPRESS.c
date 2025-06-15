c339c9663d22c9cd5280c8e337cef9a24104783b
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-06-15 11:02:29 +0200

    Decomp

--- src/TITLE/TITLE.PRG/libpress/LIBPRESS.c
+++ src/TITLE/TITLE.PRG/libpress/LIBPRESS.c
@@ -2,11 +2,68 @@
 #include <libpress.h>
 #include <libetc.h>
 
-INCLUDE_ASM("build/src/TITLE/TITLE.PRG/nonmatchings/libpress/LIBPRESS", DecDCTReset);
 
-INCLUDE_ASM("build/src/TITLE/TITLE.PRG/nonmatchings/libpress/LIBPRESS", DecDCTGetEnv);
+void func_80072050(int);
+void func_80072140(int* arg0, u_int arg1);
 
-INCLUDE_ASM("build/src/TITLE/TITLE.PRG/nonmatchings/libpress/LIBPRESS", DecDCTPutEnv);
+extern int D_80075B44[];
+extern int D_80075BC8[];
+extern int D_80075B48[];
+extern int D_80075B88[];
+extern int D_80075BCC[];
+
+void DecDCTReset(int mode) {
+    if (mode == 0) {
+        ResetCallback();
+    }
+    func_80072050(mode);
+}
+
+DECDCTENV* DecDCTGetEnv(DECDCTENV* env) {
+    int* var_a2;
+    int i;
+    int* var_a1;
+
+    var_a2 = (int*)env->iq_y;
+    var_a1 = D_80075B48;
+    for (i = 15; i != -1; --i) {
+        *var_a2++ = *var_a1++;
+    }
+    var_a2 = (int*)env->iq_c;
+    var_a1 = D_80075B88;
+    for (i = 15; i != -1; --i) {
+        *var_a2++ = *var_a1++;
+    }
+    var_a2 = (int*)env->dct;
+    var_a1 = D_80075BCC;
+    for (i = 31; i != -1; --i) {
+        *var_a2++ = *var_a1++;
+    }
+    return env;
+}
+
+DECDCTENV* DecDCTPutEnv(DECDCTENV* arg0) {
+    int i;
+    int *src1, *dst1;
+    int *src2, *dst2;
+
+    dst1 = D_80075B48;
+    src1 = (int*)&arg0->iq_y;
+    for (i = 15; i != -1; i--) {
+        *dst1++ = *src1++;
+    }
+
+    dst2 = D_80075B88;
+    src2 = (int*)&arg0->iq_c;
+    for (i = 15; i != -1; i--) {
+        *dst2++ = *src2++;
+    }
+
+    func_80072140(D_80075B44, 32);
+    func_80072140(D_80075BC8, 32);
+
+    return arg0;
+}
 
 INCLUDE_ASM("build/src/TITLE/TITLE.PRG/nonmatchings/libpress/LIBPRESS", DecDCTin);
 

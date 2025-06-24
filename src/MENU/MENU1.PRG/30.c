307152b79bddfcabb17c34ed3b7da7fb1723d195
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-06-24 09:11:15 +0200

    Generate c file

---
+++ src/MENU/MENU1.PRG/30.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+INCLUDE_ASM("build/src/MENU/MENU1.PRG/nonmatchings/30", func_80102830);
+
+INCLUDE_ASM("build/src/MENU/MENU1.PRG/nonmatchings/30", func_80102914);
+
+INCLUDE_ASM("build/src/MENU/MENU1.PRG/nonmatchings/30", func_80102A4C);
+
+INCLUDE_ASM("build/src/MENU/MENU1.PRG/nonmatchings/30", func_80102F68);
+
+INCLUDE_ASM("build/src/MENU/MENU1.PRG/nonmatchings/30", func_801031FC);
+
+INCLUDE_ASM("build/src/MENU/MENU1.PRG/nonmatchings/30", func_80103270);

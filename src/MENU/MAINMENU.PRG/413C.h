5a2f3fce019f81df4af2788dc30473347ccbe516
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-07-11 17:51:15 +0200

    Decomp

--- src/MENU/MAINMENU.PRG/413C.h
+++ src/MENU/MAINMENU.PRG/413C.h
@@ -5,7 +5,7 @@ void func_800FFA94();
 void func_800FFB68(int);
 void func_800FFBA8();
 void func_800FFBC8();
-void func_80100100(int, signed char*, int, int);
+void func_80100100(int, char const*, int, int);
 void func_80100164();
 void func_80100414(int, int);
 void func_801005E0(int, int, u_short**, int*);
--- src/MENU/MENU1.PRG/30.c
+++ src/MENU/MENU1.PRG/30.c
@@ -1,8 +1,32 @@
 #include "common.h"
 #include "../MAINMENU.PRG/413C.h"
+#include "../../BATTLE/BATTLE.PRG/146C.h"
 #include "../../BATTLE/BATTLE.PRG/5BF94.h"
 
-INCLUDE_ASM("build/src/MENU/MENU1.PRG/nonmatchings/30", func_80102830);
+extern char D_8010451C[];
+
+void func_80102830(int arg0) {
+    int temp_v0;
+    int var_s1;
+    int var_a0;
+
+    temp_v0 = func_8008A5D0(0, arg0);
+    func_80100100(1, "HP", 8, (temp_v0 >> 1) & 1);
+    var_s1 = 0xF;
+    var_a0 = vs_main_skills[arg0].cost;
+    D_8010451C[15] = 0;
+    
+    do {
+        --var_s1;
+        var_a0 = func_800CCC54(var_a0);
+        D_8010451C[var_s1] = (var_a0 & 0xF) + 0x30;
+        var_a0 >>= 4;
+    } while (var_a0 != 0);
+    
+    --var_s1;
+    D_8010451C[var_s1] = 0x23;
+    func_80100100(0, &D_8010451C[var_s1], 0x48, (temp_v0 >> 1) & 1);
+}
 
 INCLUDE_ASM("build/src/MENU/MENU1.PRG/nonmatchings/30", func_80102914);
 

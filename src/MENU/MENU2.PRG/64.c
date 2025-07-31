336e5d94c2656aa143c5c59105e9ffb448c948c9
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-07-31 22:26:12 +0200

    Decomp func_80103DD8

--- src/MENU/MENU2.PRG/64.c
+++ src/MENU/MENU2.PRG/64.c
@@ -1,5 +1,9 @@
 #include "common.h"
 
+extern u_short D_80104690[];
+extern char D_8010505A[];
+extern char D_80105078[];
+
 INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80102864);
 
 INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80102928);
@@ -18,7 +22,15 @@ INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_801038D4);
 
 INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80103C3C);
 
-INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80103DD8);
+char* func_80103DD8(int arg0) {
+    u_char temp_v1;
+
+    temp_v1 = D_80105078[arg0];
+    if (D_80105078[arg0] == 0) {
+        return D_8010505A;
+    }
+    return (char*)&D_80104690[D_80104690[temp_v1]];
+}
 
 INCLUDE_ASM("build/src/MENU/MENU2.PRG/nonmatchings/64", func_80103E20);
 

1d9cb5dc0c47e06e7d02f5dbd2b71d50cadebf64
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-09-01 08:14:11 +0200

    Decomp

--- src/MENU/MENU8.PRG/88.c
+++ src/MENU/MENU8.PRG/88.c
@@ -17,7 +17,66 @@ extern char D_80105DB2;
 extern char D_80105F40[];
 extern char D_80105F53;
 
-INCLUDE_ASM("build/src/MENU/MENU8.PRG/nonmatchings/88", func_80102888);
+extern char D_80105D64[];
+extern int D_80105D68;
+extern int D_80105D6C;
+
+/*
+                    do {
+                    if ((u8) vs_main_settings.simpleMap == *(var_a1_2 + &D_80105D64)) {
+                        *var_a0 |= 4;
+                    }
+                    var_a1_2 += 1;
+                    var_a0 += 4;
+                } while (var_a1_2 < 3);
+*/
+int func_80102888(int arg0) {
+    char* menuStrings[6];
+    int rowTypes[3];
+    int i;
+
+    if (arg0 != 0) {
+        D_80105D68 = 0;
+        return 0;
+    }
+    switch (D_80105D68) {
+    case 0:
+        if (vs_mainmenu_ready() != 0) {
+            for (i = 0; i < 3; ++i) {
+                menuStrings[i * 2] = (char*)&D_80105558[D_80105558[i * 2 + 0x14]];
+                menuStrings[i * 2 + 1] = (char*)&D_80105558[D_80105558[i * 2 + 0x15]];
+                rowTypes[i] = 0;
+            }
+            for (i = 0; i < 3; ++i) {
+                if (vs_main_settings.simpleMap == D_80105D64[i]) {
+                    rowTypes[i] |= 4;
+                }
+            }
+            vs_mainmenu_setMenuRows(3, 0x246, menuStrings, rowTypes);
+            D_80105D68 = 1;
+        }
+        break;
+    case 1:
+        D_80105D6C = vs_mainmenu_getSelectedRow() + 1;
+        if (D_80105D6C != 0) {
+            if (D_80105D6C == -2) {
+                func_800FA8E0(0x28);
+                func_800FFBA8();
+                func_800FFA88(0);
+            } else {
+                func_800FA8E0(7);
+            }
+            D_80105D68 = 2;
+        }
+        break;
+    case 2:
+        if (vs_mainmenu_ready() != 0) {
+            return D_80105D6C;
+        }
+        break;
+    }
+    return 0;
+}
 
 extern int D_80105D70;
 extern int D_80105D74;

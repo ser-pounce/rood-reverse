33725ff72154e2efc8ea6a770cc429a72fc51000
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-09-01 07:52:09 +0200

    Decomp

--- src/MENU/MENU8.PRG/88.c
+++ src/MENU/MENU8.PRG/88.c
@@ -19,7 +19,53 @@ extern char D_80105F53;
 
 INCLUDE_ASM("build/src/MENU/MENU8.PRG/nonmatchings/88", func_80102888);
 
-INCLUDE_ASM("build/src/MENU/MENU8.PRG/nonmatchings/88", func_80102A5C);
+extern int D_80105D70;
+extern int D_80105D74;
+
+int func_80102A5C(int arg0) {
+    char* menuStrings[4];
+    int rowTypes[2];
+    int i;
+
+    if (arg0 != 0) {
+        D_80105D70 = 0;
+        return 0;
+    }
+    switch (D_80105D70) {
+    case 0:
+        if (vs_mainmenu_ready() != 0) {
+            for (i = 0; i < 2; ++i) {
+                menuStrings[i * 2] = (char*)&D_80105558[D_80105558[i * 2 + 0x1A]];
+                menuStrings[i * 2 + 1] = (char*)&D_80105558[D_80105558[i * 2 + 0x1B]];
+                rowTypes[i] = 0;
+            }
+            i = 1 - ((*(u_int*)&vs_main_settings >> 5) & 1);
+            rowTypes[i] |= 4;
+            vs_mainmenu_setMenuRows(2, 0x247, menuStrings, rowTypes);
+            D_80105D70 = 1;
+        }
+        break;
+    case 1:
+        D_80105D74 = vs_mainmenu_getSelectedRow() + 1;
+        if (D_80105D74 != 0) {
+            if (D_80105D74 == -2) {
+                func_800FA8E0(0x28);
+                func_800FFBA8();
+                func_800FFA88(0);
+            } else {
+                func_800FA8E0(7);
+            }
+            D_80105D70 = 2;
+        }
+        break;
+    case 2:
+        if (vs_mainmenu_ready() != 0) {
+            return D_80105D74;
+        }
+        break;
+    }
+    return 0;
+}
 
 extern int D_80105D78;
 extern int D_80105D7C;

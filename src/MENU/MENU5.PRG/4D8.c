a290eba7258998a91da9bacf70f17a7c89c988db
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-10-21 18:49:39 +0200

    Decomp

--- src/MENU/MENU5.PRG/4D8.c
+++ src/MENU/MENU5.PRG/4D8.c
@@ -2,6 +2,7 @@
 #include "../MAINMENU.PRG/C48.h"
 #include "../MAINMENU.PRG/413C.h"
 #include "../MAINMENU.PRG/8170.h"
+#include "../../BATTLE/BATTLE.PRG/5BF94.h"
 
 void func_80102CD8(int arg0)
 {
@@ -18,9 +19,31 @@ INCLUDE_ASM("build/src/MENU/MENU5.PRG/nonmatchings/4D8", func_80102D1C);
 
 INCLUDE_ASM("build/src/MENU/MENU5.PRG/nonmatchings/4D8", func_80102D90);
 
-INCLUDE_ASM("build/src/MENU/MENU5.PRG/nonmatchings/4D8", func_80102ED8);
+int func_80102ED8(void)
+{
+    int temp_v0;
 
-INCLUDE_ASM("build/src/MENU/MENU5.PRG/nonmatchings/4D8", func_80102F30);
+    temp_v0 = vs_mainmenu_getSelectedRow();
+    if (temp_v0 < -1) {
+        func_800FA8E0(5);
+        return temp_v0;
+    }
+    if (temp_v0 >= 0) {
+        func_800FA92C(D_800F4EE8.unk0[0], 1);
+    }
+    return temp_v0;
+}
+
+void func_80102F30(char* text, char arg1)
+{
+    vs_battle_menuItem_t* menuItem;
+
+    menuItem = vs_battle_setMenuItem(10, 320, 34, 0x8C, 8, text);
+    menuItem->state = 2;
+    menuItem->x = 0xB4;
+    menuItem->selected = 1;
+    menuItem->unkA = arg1;
+}
 
 void func_80102F8C(void) { func_800FA8E0(5); }
 

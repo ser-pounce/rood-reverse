265993e53d318e6e49efa88e45e3f65105b250db
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-11-14 07:39:32 +0100

    Remerge for now

--- src/MENU/MENU9.PRG/238.c
+++ src/MENU/MENU9.PRG/238.c
@@ -1,5 +1,14 @@
 #include "common.h"
 #include "../MAINMENU.PRG/C48.h"
+#include "../../SLUS_010.40/main.h"
+#include "../../BATTLE/BATTLE.PRG/2842C.h"
+
+void func_801084E4();
+
+extern short D_801098A4[];
+extern u_int D_801098C4[];
+extern int D_8010A460;
+extern short D_8010A464;
 
 INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_80102A38);
 
@@ -98,3 +107,43 @@ INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_80107090);
 INCLUDE_RODATA("build/src/MENU/MENU9.PRG/nonmatchings/238", D_801029AC);
 
 INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_80107120);
+
+void func_80107600(void) {
+    int i;
+    int var_a2;
+    int a1;
+    
+    for (i = 0, var_a2 = 0, a1 = 1; i < 16; ++i) {
+        if (D_8005FEA0.unk0[0] & (a1 << i)) {
+            ++var_a2;
+        }
+    }
+
+    for (i = 0; i < 16; ++i) {
+        if ((var_a2 >= D_801098A4[i]) && (D_8010A460 >= D_801098C4[i])) {
+            D_8010A464 = i;
+            return;
+        }
+    }
+}
+
+
+INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_801076A4);
+
+INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_80107A98);
+
+INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_80107C34);
+
+INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_80107FBC);
+
+INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_80108098);
+
+INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_801080C8);
+
+INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_8010812C);
+
+INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_8010823C);
+
+INCLUDE_ASM("build/src/MENU/MENU9.PRG/nonmatchings/238", func_801084E4);
+
+void func_80108514(void) { }

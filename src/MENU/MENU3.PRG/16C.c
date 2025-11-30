7f09e4c0bee1c7fe3b8ddea0727c9d9d739cb639
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-11-30 20:30:11 +0100

    Split file

--- src/MENU/MENU3.PRG/16C.c
+++ src/MENU/MENU3.PRG/16C.c
@@ -1199,15 +1199,3 @@ int func_80108970(char* arg0)
     }
     return 0;
 }
-
-// https://decomp.me/scratch/JlHtx
-// 2.7.2-cdk
-INCLUDE_ASM("build/src/MENU/MENU3.PRG/nonmatchings/16C", func_801090C4);
-
-// https://decomp.me/scratch/g9L6W
-// 2.7.2-cdk
-INCLUDE_ASM("build/src/MENU/MENU3.PRG/nonmatchings/16C", func_80109320);
-
-// https://decomp.me/scratch/3TToS
-// 2.7.2-cdk
-INCLUDE_ASM("build/src/MENU/MENU3.PRG/nonmatchings/16C", func_80109344);
---
+++ src/MENU/MENU3.PRG/68C4.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+// https://decomp.me/scratch/JlHtx
+// 2.7.2-cdk
+INCLUDE_ASM("build/src/MENU/MENU3.PRG/nonmatchings/68C4", func_801090C4);
+
+// https://decomp.me/scratch/g9L6W
+// 2.7.2-cdk
+INCLUDE_ASM("build/src/MENU/MENU3.PRG/nonmatchings/68C4", func_80109320);
+
+// https://decomp.me/scratch/3TToS
+// 2.7.2-cdk
+INCLUDE_ASM("build/src/MENU/MENU3.PRG/nonmatchings/68C4", func_80109344);

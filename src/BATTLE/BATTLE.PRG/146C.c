ba224bb90110c687bd445487692a3b7500352303
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2025-12-05 13:41:13 +0100

    Decomp func_800840EC

--- src/BATTLE/BATTLE.PRG/146C.c
+++ src/BATTLE/BATTLE.PRG/146C.c
@@ -1868,7 +1868,12 @@ INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/146C", func_8008404C);
 
 INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/146C", func_8008409C);
 
-INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/146C", func_800840EC);
+int func_800840EC(int arg0 __attribute__((unused)), int arg1 __attribute__((unused)), func_80085718_t* arg2) 
+{
+    if (func_800810CC(0x10, arg2) != 0) {
+        arg2->unk1C = (arg2->unk1C & 0xFFFCFFFF) | 0x20000;
+    }
+}
 
 INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/146C", func_8008413C);
 

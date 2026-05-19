40cbee6d5fa96d0666339e7d1981819ab4153e70
andreadellacorte <andrea@dellacorte.me>
2026-05-19 21:58:11 +0100

    Address flake review comments

--- src/include/lbaMacros.h
+++ src/include/lbaMacros.h
@@ -2,5 +2,8 @@
 
 #define squashLba(file) squashLbaVal((file##_LBA), (file##_SIZE))
 #define squashLbaVal(lba, size) ((lba) << 8 | ((size) >> 11))
-#define mkCdFile(file) { VS_##file##_LBA, VS_##file##_SIZE }
+#define mkCdFile(file)                                                                   \
+    {                                                                                    \
+        VS_##file##_LBA, VS_##file##_SIZE                                                \
+    }
 #define mkHfoPair(file) mkCdFile(file##_HF1), mkCdFile(file##_HF0)

e12b485f8b91091ed86d1ea6851dced07335a8ec
andreadellacorte <andrea@dellacorte.me>
2026-05-19 10:34:24 +0100

    format: re-run make format

--- src/include/lbaMacros.h
+++ src/include/lbaMacros.h
@@ -2,8 +2,5 @@
 
 #define squashLba(file) squashLbaVal((file##_LBA), (file##_SIZE))
 #define squashLbaVal(lba, size) ((lba) << 8 | ((size) >> 11))
-#define mkCdFile(file)                                                                   \
-    {                                                                                    \
-        VS_##file##_LBA, VS_##file##_SIZE                                                \
-    }
+#define mkCdFile(file) { VS_##file##_LBA, VS_##file##_SIZE }
 #define mkHfoPair(file) mkCdFile(file##_HF1), mkCdFile(file##_HF0)

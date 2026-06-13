ce87b9dc455d202931213d210278c6698e435863
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2026-06-13 15:38:21 +0200

    Refactor

--- src/MENU/MENU0.PRG/84.h
+++ src/MENU/MENU0.PRG/84.h
@@ -1,3 +1,13 @@
 #pragma once
+#include <stddef.h>
 
-int vs_menu0_exec(char* state);
+void _setMPCostDirect(int costDecimal, int disabled);
+int _getTeleportCost(int targetSavePointId);
+int _warlockMagicMenu(u_int initCursorMemory);
+int _shamanMagicMenu(u_int initCursorMemory);
+int _sorcererMagicMenu(u_int initCursorMemory);
+int _enchanterMagicMenu(u_int initCursorMemory);
+
+extern u_short _magicStrings[];
+extern u_short _teleportationStrings[];
+extern int _savePointData[];

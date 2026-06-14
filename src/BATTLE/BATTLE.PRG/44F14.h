4bcceb776239a93fb511715a4001391278e86534
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-06-14 10:22:08 -0700

    Decomp func_800AF6E8

--- src/BATTLE/BATTLE.PRG/44F14.h
+++ src/BATTLE/BATTLE.PRG/44F14.h
@@ -7,5 +7,5 @@ void func_800AEAE8(void*);
 void func_800AECA0(void*);
 void func_800AF6E8(void*);
 void func_800AFA28(D_800F4538_t*, D_800F4538_unkC54*, int);
-void func_800B002C(void*, int);
+void func_800B002C();
 void func_800B217C(void*, void*);

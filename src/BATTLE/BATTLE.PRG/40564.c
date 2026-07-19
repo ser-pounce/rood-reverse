e3140e70c3aa4adeeaa7c39773465957ee4f58a8
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2026-07-19 20:52:35 +0200

    Decomp BATTLE 40564 func_800A9E38

--- src/BATTLE/BATTLE.PRG/40564.c
+++ src/BATTLE/BATTLE.PRG/40564.c
@@ -109,7 +109,23 @@ void func_800A9D24(int arg0, SVECTOR* arg1, int arg2)
 
 INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/40564", func_800A9D90);
 
-INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/40564", func_800A9E38);
+void func_800A9E38(int arg0, int arg1, int arg2)
+{
+    u_short temp_s0;
+    D_800F4538_t* temp_s1 = D_800F4538[arg0];
+
+    if (arg2 == -1) {
+        func_800AA984(arg0, arg1, -1);
+        return;
+    }
+
+    temp_s0 = temp_s1->unk5C0;
+    temp_s1->unk5C0 = arg2;
+
+    func_800AA984(arg0, arg1, -1);
+
+    temp_s1->unk5C0 = temp_s0;
+}
 
 INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/40564", func_800A9EB4);
 

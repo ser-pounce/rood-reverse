786e0ffe62091b18c063c195e3816d579b2940b9
Luis Del Mar <ladelmar99@gmail.com>
2026-08-13 00:27:15 -0600

    made suggested changes

--- src/BATTLE/BATTLE.PRG/5BF94.c
+++ src/BATTLE/BATTLE.PRG/5BF94.c
@@ -3818,10 +3818,9 @@ int func_800D61AC(D_800F53B8_t* arg0, func_800D5780_t* arg1)
 
     if (temp_s2->unk0 == arg0->unkD1C.unk30->unk2) {
         for (i = 0; i < D_800F5230.unk3; ++i) {
-            if (func_800A0BE0((int)(u_char)D_800F5230.unkD0[i]) & 2) {
-                func_8007B29C((int)temp_s2->unk7, (int)temp_s2->unk2,
-                    (int)(u_char)D_800F5230.unkD0[i], temp_s2->unk4 - 0x80,
-                    (short)(temp_s2->unk5 - 0x80), (short)(temp_s2->unk6 - 0x80));
+            if (func_800A0BE0(D_800F5230.unkD0[i]) & 2) {
+                func_8007B29C(temp_s2->unk7, temp_s2->unk2, D_800F5230.unkD0[i],
+                    temp_s2->unk4 - 0x80, (temp_s2->unk5 - 0x80), (temp_s2->unk6 - 0x80));
             }
         }
         return func_800D5780(arg1);

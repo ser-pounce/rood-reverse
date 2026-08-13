803201b810cd6f73e8380ba96e8249bd40259a51
Luis Del Mar <77761706+ladm99@users.noreply.github.com>
2026-08-13 00:30:19 -0600

    func_800D61AC decomp (#54)
    * decomp for func_800D278C in BATTLE/BATTLE.PRG/5BF94.c
    
    * forgot to run make format before pushing up
    
    * changed changed D_800F5600 -= 1 to --D_800F5600
    
    * decomp of func_800D65D8 in BATTLE/BATTLE.PRG/5BF9f.c
    
    * made requested changes
    
    * decomp of func_800D61AC
    
    * forgot to run make format
    
    * made suggested changes

--- src/BATTLE/BATTLE.PRG/5BF94.c
+++ src/BATTLE/BATTLE.PRG/5BF94.c
@@ -3811,7 +3811,22 @@ int func_800D5F8C(D_800F53B8_t* arg0, func_800D5780_t* arg1)
 
 INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/5BF94", func_800D6048);
 
-INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/5BF94", func_800D61AC);
+int func_800D61AC(D_800F53B8_t* arg0, func_800D5780_t* arg1)
+{
+    int i;
+    func_800D6310_t* temp_s2 = (func_800D6310_t*)(arg1->unk0 + (arg1->unk6 * 2));
+
+    if (temp_s2->unk0 == arg0->unkD1C.unk30->unk2) {
+        for (i = 0; i < D_800F5230.unk3; ++i) {
+            if (func_800A0BE0(D_800F5230.unkD0[i]) & 2) {
+                func_8007B29C(temp_s2->unk7, temp_s2->unk2, D_800F5230.unkD0[i],
+                    temp_s2->unk4 - 0x80, (temp_s2->unk5 - 0x80), (temp_s2->unk6 - 0x80));
+            }
+        }
+        return func_800D5780(arg1);
+    }
+    return 1;
+}
 
 int func_800D6298(D_800F53B8_t* arg0, func_800D5780_t* arg1)
 {

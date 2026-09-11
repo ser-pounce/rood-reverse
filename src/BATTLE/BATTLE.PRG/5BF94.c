2dc937f510af7bb2c098632bd20e6795e0346812
Mauro <aye.mauro@gmail.com>
2026-09-11 10:43:00 -0500

    Decomp func_800D169C

--- src/BATTLE/BATTLE.PRG/5BF94.c
+++ src/BATTLE/BATTLE.PRG/5BF94.c
@@ -316,7 +316,7 @@ void func_800CFE98(SVECTOR* arg0, func_800CFE98_t* arg1);
 void func_800D0984(int, void*, int);
 void func_800D0D08(D_800F53B8_t*);
 void func_800D1104(int);
-void func_800D169C(int, int, int, int);
+void func_800D169C(SVECTOR*, SVECTOR*, int, SVECTOR*);
 void func_800D17A8(int, int, int, int);
 void func_800D1B18(D_800F54D8_t*);
 int func_800D12D8(int);
@@ -3444,14 +3444,19 @@ int vs_battle_lerp(int arg0, int arg1, int arg2)
     return arg0 + (var_v0 / (ONE * 2));
 }
 
-INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/5BF94", func_800D169C);
+void func_800D169C(SVECTOR* arg0, SVECTOR* arg1, int arg2, SVECTOR* arg3)
+{
+    arg3->vx = vs_battle_lerp(arg0->vx, arg1->vx, arg2);
+    arg3->vy = vs_battle_lerp(arg0->vy, arg1->vy, arg2);
+    arg3->vz = vs_battle_lerp(arg0->vz, arg1->vz, arg2);
+}
 
-void func_800D1718(int arg0, int arg1, int arg2, int arg3, int arg4)
+void func_800D1718(SVECTOR* arg0, SVECTOR* arg1, int arg2, int arg3, SVECTOR* arg4)
 {
     func_800D169C(arg0, arg1, ONE - rcos((arg3 * ONE / 2) / arg2), arg4);
 }
 
-void func_800D1778(int arg0, int arg1, int arg2, int arg3, int arg4)
+void func_800D1778(SVECTOR* arg0, SVECTOR* arg1, int arg2, int arg3, SVECTOR* arg4)
 {
     func_800D169C(arg0, arg1, (arg3 * (ONE * 2)) / arg2, arg4);
 }

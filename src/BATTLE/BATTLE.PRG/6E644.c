b27e5a25809e5022fa20e370f00d278871c1e548
Mauro <aye.mauro@gmail.com>
2026-09-12 10:30:34 -0500

    Decomp func_800D8038

--- src/BATTLE/BATTLE.PRG/6E644.c
+++ src/BATTLE/BATTLE.PRG/6E644.c
@@ -60,7 +60,9 @@ void func_800D7FFC(int arg0) { D_800F5638 = arg0; }
 
 INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/6E644", func_800D8008);
 
-INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/6E644", func_800D8038);
+extern int D_800F5680;
+
+void func_800D8038(int arg0) { D_800F5680 = arg0; }
 
 INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/6E644", func_800D8044);
 

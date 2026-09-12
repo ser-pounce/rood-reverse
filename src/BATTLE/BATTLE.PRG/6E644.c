39496fbff2fb4d926f12eeaf610c353bf044b256
Mauro <aye.mauro@gmail.com>
2026-09-12 10:39:42 -0500

    Decomp func_800D8200

--- src/BATTLE/BATTLE.PRG/6E644.c
+++ src/BATTLE/BATTLE.PRG/6E644.c
@@ -66,7 +66,7 @@ INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/6E644", func_800D8054);
 
 INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/6E644", func_800D8060);
 
-INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/6E644", func_800D8200);
+int func_800D8200(void) { return 0x14C08; }
 
 INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/6E644", func_800D820C);
 

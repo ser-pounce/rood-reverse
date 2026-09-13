08aa40b6a6c000636c25743aeedc3d4c7303fe20
Mauro <aye.mauro@gmail.com>
2026-09-13 13:43:55 -0500

    Decomp func_800D78CC

--- src/BATTLE/BATTLE.PRG/6E644.c
+++ src/BATTLE/BATTLE.PRG/6E644.c
@@ -53,7 +53,7 @@ extern D_800F569C_t* D_800F5698;
 
 void func_800D78B8(void) { D_800F5698 = D_800F569C; }
 
-INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/6E644", func_800D78CC);
+void func_800D78CC(void) { D_800F569C = D_800F5698; }
 
 void func_800D78E0(u_char* arg0) { D_800F569C->unk8 = arg0; }
 

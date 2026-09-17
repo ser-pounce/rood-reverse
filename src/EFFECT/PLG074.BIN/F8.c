d3e782a110494b134551516f93f154e42f68f6a0
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2026-09-17 17:07:28 +0200

    Split and dedupe PLG073

---
+++ src/EFFECT/PLG074.BIN/F8.c
@@ -0,0 +1,23 @@
+#include "common.h"
+
+INCLUDE_ASM("build/src/EFFECT/PLG074.BIN/nonmatchings/F8", func_800F98F8);
+
+INCLUDE_ASM("build/src/EFFECT/PLG074.BIN/nonmatchings/F8", func_800FA7F0);
+
+INCLUDE_ASM("build/src/EFFECT/PLG074.BIN/nonmatchings/F8", func_800FB414);
+
+INCLUDE_ASM("build/src/EFFECT/PLG074.BIN/nonmatchings/F8", func_800FBBB0);
+
+INCLUDE_ASM("build/src/EFFECT/PLG074.BIN/nonmatchings/F8", func_800FBDC8);
+
+INCLUDE_ASM("build/src/EFFECT/PLG074.BIN/nonmatchings/F8", func_800FC28C);
+
+INCLUDE_ASM("build/src/EFFECT/PLG074.BIN/nonmatchings/F8", func_800FC404);
+
+INCLUDE_ASM("build/src/EFFECT/PLG074.BIN/nonmatchings/F8", func_800FC590);
+
+INCLUDE_ASM("build/src/EFFECT/PLG074.BIN/nonmatchings/F8", func_800FCFEC);
+
+INCLUDE_ASM("build/src/EFFECT/PLG074.BIN/nonmatchings/F8", func_800FD534);
+
+INCLUDE_ASM("build/src/EFFECT/PLG074.BIN/nonmatchings/F8", func_800FE3EC);

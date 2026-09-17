5acccdaff2fdeab1a532011159bf24d684557105
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2026-09-17 18:44:05 +0200

    Split PLG086

---
+++ src/EFFECT/PLG086.BIN/F8.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+INCLUDE_ASM("build/src/EFFECT/PLG086.BIN/nonmatchings/F8", func_800F98F8);
+
+INCLUDE_ASM("build/src/EFFECT/PLG086.BIN/nonmatchings/F8", func_800FA868);
+
+INCLUDE_ASM("build/src/EFFECT/PLG086.BIN/nonmatchings/F8", func_800FB720);
+
+INCLUDE_ASM("build/src/EFFECT/PLG086.BIN/nonmatchings/F8", func_800FC270);
+
+INCLUDE_ASM("build/src/EFFECT/PLG086.BIN/nonmatchings/F8", func_800FCA0C);
+
+INCLUDE_ASM("build/src/EFFECT/PLG086.BIN/nonmatchings/F8", func_800FCC24);
+
+INCLUDE_ASM("build/src/EFFECT/PLG086.BIN/nonmatchings/F8", func_800FD0E8);
+
+INCLUDE_ASM("build/src/EFFECT/PLG086.BIN/nonmatchings/F8", func_800FD260);
+
+INCLUDE_ASM("build/src/EFFECT/PLG086.BIN/nonmatchings/F8", func_800FD3EC);
+
+INCLUDE_ASM("build/src/EFFECT/PLG086.BIN/nonmatchings/F8", func_800FDE48);

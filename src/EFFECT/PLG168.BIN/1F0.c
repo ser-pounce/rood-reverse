4a735462ac5ab4bb8b904434c5646c143b7d9655
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2026-09-18 10:34:26 +0200

    Split PLG168 PLG169 PLG170

---
+++ src/EFFECT/PLG168.BIN/1F0.c
@@ -0,0 +1,27 @@
+#include "common.h"
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800F99F0);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FA18C);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FA3A4);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FA87C);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FA9F4);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FAB80);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FB5D0);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FBB10);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FC298);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FC4B0);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FC960);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FCE90);
+
+INCLUDE_ASM("build/src/EFFECT/PLG168.BIN/nonmatchings/1F0", func_800FD104);
---
+++ src/EFFECT/PLG169.BIN/E0.c
@@ -0,0 +1,19 @@
+#include "common.h"
+
+INCLUDE_ASM("build/src/EFFECT/PLG169.BIN/nonmatchings/E0", func_800F98E0);
+
+INCLUDE_ASM("build/src/EFFECT/PLG169.BIN/nonmatchings/E0", func_800FA07C);
+
+INCLUDE_ASM("build/src/EFFECT/PLG169.BIN/nonmatchings/E0", func_800FA294);
+
+INCLUDE_ASM("build/src/EFFECT/PLG169.BIN/nonmatchings/E0", func_800FA76C);
+
+INCLUDE_ASM("build/src/EFFECT/PLG169.BIN/nonmatchings/E0", func_800FA8E4);
+
+INCLUDE_ASM("build/src/EFFECT/PLG169.BIN/nonmatchings/E0", func_800FAA70);
+
+INCLUDE_ASM("build/src/EFFECT/PLG169.BIN/nonmatchings/E0", func_800FB4C0);
+
+INCLUDE_ASM("build/src/EFFECT/PLG169.BIN/nonmatchings/E0", func_800FBA00);
+
+INCLUDE_ASM("build/src/EFFECT/PLG169.BIN/nonmatchings/E0", func_800FC624);

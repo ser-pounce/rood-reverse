972d988319b462ae23239a18de9e175e7e39e929
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2026-09-18 12:08:55 +0200

    Split PLG263

--- README.md
+++ README.md
@@ -95,6 +95,7 @@ Thanks also to the following amazing projects (and their dependencies):
 ![PLG250.BIN progress](https://decomp.dev/ser-pounce/rood-reverse.svg?mode=shield&category=effects.PLG250.BIN)
 ![PLG255.BIN progress](https://decomp.dev/ser-pounce/rood-reverse.svg?mode=shield&category=effects.PLG255.BIN)
 ![PLG260.BIN progress](https://decomp.dev/ser-pounce/rood-reverse.svg?mode=shield&category=effects.PLG260.BIN)
+![PLG263.BIN progress](https://decomp.dev/ser-pounce/rood-reverse.svg?mode=shield&category=effects.PLG263.BIN)
 
 ### Menus
 ![Total menu progress](https://decomp.dev/ser-pounce/rood-reverse.svg?mode=shield&category=menus&color=%2332a934)
---
+++ src/EFFECT/PLG263.BIN/110.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+INCLUDE_ASM("build/src/EFFECT/PLG263.BIN/nonmatchings/110", func_800F9910);
+
+INCLUDE_ASM("build/src/EFFECT/PLG263.BIN/nonmatchings/110", func_800FA4A8);
+
+INCLUDE_ASM("build/src/EFFECT/PLG263.BIN/nonmatchings/110", func_800FB0CC);
+
+INCLUDE_ASM("build/src/EFFECT/PLG263.BIN/nonmatchings/110", func_800FC00C);
+
+INCLUDE_ASM("build/src/EFFECT/PLG263.BIN/nonmatchings/110", func_800FC794);
+
+INCLUDE_ASM("build/src/EFFECT/PLG263.BIN/nonmatchings/110", func_800FC9AC);
+
+INCLUDE_ASM("build/src/EFFECT/PLG263.BIN/nonmatchings/110", func_800FCE5C);
+
+INCLUDE_ASM("build/src/EFFECT/PLG263.BIN/nonmatchings/110", func_800FDA1C);

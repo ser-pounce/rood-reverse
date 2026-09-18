d21ed416734a4c4548970b3288db998a506e915a
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2026-09-18 05:49:51 +0200

    Split PLG137

---
+++ src/EFFECT/PLG137.BIN/F8.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+INCLUDE_ASM("build/src/EFFECT/PLG137.BIN/nonmatchings/F8", func_800F98F8);
+
+INCLUDE_ASM("build/src/EFFECT/PLG137.BIN/nonmatchings/F8", func_800FA404);
+
+INCLUDE_ASM("build/src/EFFECT/PLG137.BIN/nonmatchings/F8", func_800FABA0);
+
+INCLUDE_ASM("build/src/EFFECT/PLG137.BIN/nonmatchings/F8", func_800FADB8);
+
+INCLUDE_ASM("build/src/EFFECT/PLG137.BIN/nonmatchings/F8", func_800FB27C);
+
+INCLUDE_ASM("build/src/EFFECT/PLG137.BIN/nonmatchings/F8", func_800FB3F4);
+
+INCLUDE_ASM("build/src/EFFECT/PLG137.BIN/nonmatchings/F8", func_800FB580);
+
+INCLUDE_ASM("build/src/EFFECT/PLG137.BIN/nonmatchings/F8", func_800FBFDC);
+
+INCLUDE_ASM("build/src/EFFECT/PLG137.BIN/nonmatchings/F8", func_800FC524);
+
+INCLUDE_ASM("build/src/EFFECT/PLG137.BIN/nonmatchings/F8", func_800FD074);

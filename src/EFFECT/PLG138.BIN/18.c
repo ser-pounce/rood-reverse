72a762c0d70556fd06d647c44e5530d5f19c15b7
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2026-09-18 05:53:32 +0200

    Split PLG138

---
+++ src/EFFECT/PLG138.BIN/18.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+INCLUDE_ASM("build/src/EFFECT/PLG138.BIN/nonmatchings/18", func_800F9818);
+
+INCLUDE_ASM("build/src/EFFECT/PLG138.BIN/nonmatchings/18", func_800FA6D0);
+
+INCLUDE_ASM("build/src/EFFECT/PLG138.BIN/nonmatchings/18", func_800FB2F4);
+
+INCLUDE_ASM("build/src/EFFECT/PLG138.BIN/nonmatchings/18", func_800FBE44);

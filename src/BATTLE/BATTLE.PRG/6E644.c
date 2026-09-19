72e2077d95fd4c1eebe58d6dfc677d8ca066dbb2
Luis Del Mar <77761706+ladm99@users.noreply.github.com>
2026-09-19 00:33:12 -0600

    func_800D7A90 decomp (#123)
    * decomp for func_800D278C in BATTLE/BATTLE.PRG/5BF94.c
    
    * forgot to run make format before pushing up
    
    * changed changed D_800F5600 -= 1 to --D_800F5600
    
    * decomp of func_800D65D8 in BATTLE/BATTLE.PRG/5BF9f.c
    
    * made requested changes
    
    * decomp of func_800D61AC
    
    * forgot to run make format
    
    * made suggested changes
    
    * decomp of func_800CF92C
    
    * decomp of func_800D7A90
    
    * forgot to run make format
    
    * Revert "forgot to run make format"
    
    This reverts commit 284b579b617b0aa43eb451315736784c36327da4.
    
    reverting commit do to failed check
    
    * only pushing the format for 6E644.c
    
    * Update 6E644.c

--- src/BATTLE/BATTLE.PRG/6E644.c
+++ src/BATTLE/BATTLE.PRG/6E644.c
@@ -172,7 +172,10 @@ void func_800D7A74(u_short* arg0)
     D_800F569C->unkB0 = arg0;
 }
 
-INCLUDE_ASM("build/src/BATTLE/BATTLE.PRG/nonmatchings/6E644", func_800D7A90);
+void* func_800D7A90(int arg0)
+{
+    return (u_char*)D_800F569C->unkB0 + D_800F569C->unkB0[arg0 + 2];
+}
 
 void func_800D7AB4(u_char* arg0) { D_800F569C->unkC0 = arg0; }
 
